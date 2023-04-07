f465ae9f350573ba1a707c755d2a13182e33c203
MUHAMMADARSALAN041002 <muhammad.arslan@bssuniversal.com>
2023-04-07 11:57:49 +0500

    solved cors issue

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use("/countries", require("./routes/country"));
 app.use("/states", require("./routes/state"));
 app.use("/cities", require("./routes/city"));
